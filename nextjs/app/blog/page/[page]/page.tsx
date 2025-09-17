fb69d83473cdaddd5555f84811d1cbe309a41ebb
Abdullah Harits <harits.abdullah19@gmail.com>
2025-09-17 14:03:36 +0700

    reset nextjs

--- nextjs/app/blog/page/[page]/page.tsx
+++
@@ -1,8 +0,0 @@
-import BlogPage from 'app/blog/page'
-
-export default async function Page(props: { params: Promise<{ page: string }> }) {
-  const params = await props.params
-  const pageNumber = parseInt(params.page as string)
-
-  return <BlogPage page={pageNumber} />
-}
